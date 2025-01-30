5554994dc78ea27d4ee2fb4c5435d8bf26d0e275
abdulgafar <abdulgafarbusari49@gmail.com>
2025-01-30 04:01:33 -0800

    added task 0

---
+++ Task-0/src/index.mjs
@@ -0,0 +1,30 @@
+import express from "express"
+import cors from "cors";
+import dotenv from "dotenv";
+
+
+dotenv.config();
+
+
+const app = express()
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+
+
+app.get("/",(request, response) => {
+    response.status(200).json({
+        email: "abdulgafarbusari49@gmail.com",
+        current_datetime: new Date().toISOString(),
+        github_url: "https://github.com/abdulgafar01/HNG-backend.git"
+      });
+})
+
+
+app.listen(PORT, ()=>{
+    console.log(`running on port ${PORT}`)
+})
+
